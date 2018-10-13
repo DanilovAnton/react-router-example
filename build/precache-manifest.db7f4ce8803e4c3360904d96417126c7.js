1c1aa7a449b3a14f6b8f933f06f455d454424a1e
Anton <UnShock48@gmail.com>
2018-10-13 15:46:02 +0300

    Added build

---
+++ build/precache-manifest.db7f4ce8803e4c3360904d96417126c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2cd3e96b61899ba0fbba",
+    "url": "/static/js/main.2cd3e96b.chunk.js"
+  },
+  {
+    "revision": "a9d93fac72ac107f1e02",
+    "url": "/static/js/1.a9d93fac.chunk.js"
+  },
+  {
+    "revision": "2cd3e96b61899ba0fbba",
+    "url": "/static/css/main.9db25cff.chunk.css"
+  },
+  {
+    "revision": "d3fd88e114bfb64c4822ea1b030870b3",
+    "url": "/index.html"
+  }
+];
